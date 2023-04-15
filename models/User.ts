d5265976e3f38f977f274771be5cb33a4cf37324
Christian Aquino <cjfaquino@gmail.com>
2023-04-14 21:37:37 -0700

    add isAdmin & email

--- models/User.ts
+++ models/User.ts
@@ -3,8 +3,10 @@ import { model, Schema } from 'mongoose';
 const userSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
   membership: { type: Boolean, required: true, default: false },
+  isAdmin: { type: Boolean },
 });
 
 userSchema.virtual('fullName').get(function () {
