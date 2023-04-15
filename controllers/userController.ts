bfafd130ae708ab3c7d5f82056369ee06e93c9b4
Christian Aquino <cjfaquino@gmail.com>
2023-04-14 22:20:49 -0700

    add sign_up_post

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,12 +1,80 @@
 import { RequestHandler } from 'express';
+import bcryptjs from 'bcryptjs';
+import User from '../models/User';
+import { ValidationChain, body, validationResult } from 'express-validator';
 
 export const sign_up_get: RequestHandler = (req, res, next) => {
   res.render('sign-up', { title: 'Sign Up' });
 };
 
-export const sign_up_post: RequestHandler = (req, res, next) => {
-  res.send('TODO: Sign Up Post');
-};
+export const sign_up_post: [
+  ValidationChain,
+  ValidationChain,
+  ValidationChain,
+  ValidationChain,
+  ValidationChain,
+  RequestHandler
+] = [
+  // validate & sanitize fields
+  body('firstName', 'First name is required.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('lastName', 'Last name is required.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('email')
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Email is required.'),
+  body('password')
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long')
+    .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+    .withMessage('Password must contain at least one letter and one number'),
+  body('confirmPass').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Password confirmation does not match password');
+    }
+    return true;
+  }),
+
+  // process request after validation & sanitization
+  (req, res, next) => {
+    // extract validation errors from request
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // there are errors, render form again with sanitized values/ errors
+      res.render('sign-up', {
+        title: 'Sign Up',
+        user: new User(req.body),
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    // hash password field
+    bcryptjs.hash(req.body.password, 10, async (err, hashedPass) => {
+      if (err) return next(err);
+
+      // create User obj
+      const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hashedPass,
+      });
+
+      // success - redirect
+      const results = await user.save();
+      res.redirect('/');
+    });
+  },
+];
 
 export const log_in_get: RequestHandler = (req, res, next) => {
   res.send('TODO: Log In Get');
