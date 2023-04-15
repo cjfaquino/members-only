fd828296efdb6178d54da4d46d02ba1bc7d174d2
Christian Aquino <cjfaquino@gmail.com>
2023-04-14 21:28:18 -0700

    add default membership

--- models/User.ts
+++ models/User.ts
@@ -4,7 +4,7 @@ const userSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   password: { type: String, required: true },
-  membership: { type: Boolean, required: true },
+  membership: { type: Boolean, required: true, default: false },
 });
 
 userSchema.virtual('fullName').get(function () {
