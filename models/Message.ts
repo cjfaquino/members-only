15736aa9a5b6c01c2f1a69d7d08681323b01ca32
Christian Aquino <cjfaquino@gmail.com>
2023-04-15 22:23:56 -0700

    require author

--- models/Message.ts
+++ models/Message.ts
@@ -4,7 +4,7 @@ const messageSchema = new Schema({
   title: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
   text: { type: String, required: true },
-  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 export default model('Message', messageSchema);
