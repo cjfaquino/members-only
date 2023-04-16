094aa9042332c3d947eb967026e938376a2e9827
Christian Aquino <cjfaquino@gmail.com>
2023-04-15 22:05:26 -0700

    change timestamp default

--- models/Message.ts
+++ models/Message.ts
@@ -2,7 +2,7 @@ import { model, Schema } from 'mongoose';
 
 const messageSchema = new Schema({
   title: { type: String, required: true },
-  timestamp: { type: Date, required: true },
+  timestamp: { type: Date, default: Date.now },
   text: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, ref: 'User' },
 });
